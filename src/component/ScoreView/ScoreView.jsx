5700275ee665c7f12ad59491a9185d496fed8e37
no-bike <837606776@qq.com>
2024-05-07 22:56:14 +0800

    op: 美化背景

--- src/component/ScoreView/ScoreView.jsx
+++ src/component/ScoreView/ScoreView.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './ScoreView.css';
 
 export default function ScoreView() {
     return (
         <div className="score-view">
-            这个页面其实只是为了告诉你我们有路由功能哈哈
+            <div>
+                <h1>关于我们</h1>
+            这个页面其实只是为了告诉你我们有子路由功能哈哈
+            </div>
         </div>
     )
 }

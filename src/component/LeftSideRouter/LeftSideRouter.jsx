c347f4b26273915b05a561270a40a8a98c280f4c
no-bike <837606776@qq.com>
2024-05-09 16:07:08 +0800

    op: 导航栏美化

--- src/component/LeftSideRouter/LeftSideRouter.jsx
+++ src/component/LeftSideRouter/LeftSideRouter.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 import "../../Router/Routemain";
 import "./LeftSideRouter.css";
 
 export default function LeftSideRouter() {
+    const location = useLocation();
+
     return(
         <div className="left-side">
-            <Link className="sidebar-link" to="/home/scoresearch">成绩查询</Link>
-            <Link className="sidebar-link" to="/home/scoreview">关于我们</Link>
+            <NavLink className={location.pathname === "/home" ? "active-link" : "sidebar-link"} to="/home">首页</NavLink>
+            <NavLink className={location.pathname === "/home/scoresearch" ? "active-link" : "sidebar-link"} to="/home/scoresearch">成绩查询</NavLink>
+            <NavLink className={location.pathname === "/home/scoreview" ? "active-link" : "sidebar-link"} to="/home/scoreview">关于我们</NavLink>
         </div>
     );
-
 }

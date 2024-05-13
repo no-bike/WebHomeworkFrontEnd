a4aaa2930baf0bb2778f76dae52c37bc36b4415c
no-bike <837606776@qq.com>
2024-05-13 21:35:53 +0800

    首页多加了一句话= =

--- src/component/FirstPage/FirstPage.jsx
+++ src/component/FirstPage/FirstPage.jsx
@@ -4,6 +4,10 @@ export default function FirstPage() {
     return(
         <div>
             <h1>First Page</h1>
+            <div>
+                <p>This is the first page of the application</p>
+                <p>主要功能都在成绩查询里</p>
+            </div>
         </div>
     )
 }
